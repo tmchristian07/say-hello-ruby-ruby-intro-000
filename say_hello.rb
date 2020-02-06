abb0c23b2497f670cd52e715c649cac6e7ff0870
Tim Christian <tmchristian07@gmail.com>
2020-02-06 08:25:44 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 # Build your say_hello method here
 def say_hello(name)
-  puts "Hello, #{name}"
+  puts "Hello, #{name}!"
 end
 
 def say_hello()
-  puts "Hello, Ruby programmer!"
+  puts "Hello, Ruby Programmer!"
 end
